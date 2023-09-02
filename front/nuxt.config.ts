bfd194f71af1f3a432507c2c75b2d7d2ca4821ba
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-09-03 00:54:09 +0200

    Add first front

---
+++ front/nuxt.config.ts
@@ -0,0 +1,90 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+        htmlAttrs: {
+          lang: "en"
+        },
+        charset: "utf-8",
+        viewport: "width=device-width,initial-scale=1",
+        title: "Gravitalia",
+        meta: [
+          { property: "og:type", content: "website" },
+          { property: "og:site_name", content: "Gravitalia" },
+          { property: "og:title", content: "Gravitalia" },
+          { property: "og:image", content: "/favicon.webp" },
+          { name: "theme-color", content: "#332b43" },
+          { name: "robots", content: "index, follow" },
+          { name: "twitter:card", content: "summary" },
+          { name: "twitter:site", content: "@gravitalianews" }
+        ],
+        link: [
+          { rel: "icon", type: "image/webp", href: "/favicon.webp" },
+          { rel: "manifest", href: "/manifest.json" }
+        ],
+        script: [
+          { innerHTML: '"serviceWorker"in navigator&&navigator.serviceWorker.register("/sw.js",{scope:"/"});' }
+        ],
+        bodyAttrs: {
+          class: "dark:bg-zinc-900 dark:text-white"
+        }
+    }
+  },
+
+  ssr: false,
+  components: true,
+  modules: [
+      "@nuxtjs/tailwindcss",
+      ["@nuxtjs/apollo", {
+        clients: {
+          default: {
+            httpEndpoint: 'https://countries.trevorblades.com/'
+          }
+        }
+      }],
+      ["@nuxtjs/i18n", {
+        defaultLocale: "en",
+        strategy: "no_prefix",
+        lazy: false,
+        langDir: "lang",
+        detectBrowserLanguage: {
+          useCookie: true,
+          cookieKey: "lang",
+          redirectOn: "root",
+          fallbackLocale: "en",
+          alwaysRedirect: true
+        },
+        locales: [
+          {
+            code: "en",
+            iso: "en-US",
+            file: "en-US.json",
+            name: "English"
+          },
+          {
+            code: "fr",
+            iso: "fr-FR",
+            file: "fr-FR.json",
+            name: "Français"
+          }
+        ],   
+        baseUrl: "https://www.gravitalia.com"
+      }],
+      ["@nuxtjs/color-mode", {
+        preference: 'system',
+        fallback: 'light',
+        hid: 'color-script',
+        globalName: '__NUXT_COLOR_MODE__',
+        componentName: 'ColorScheme',
+        classPrefix: '',
+        classSuffix: '',
+        storageKey: 'mode'
+      }]
+  ],
+
+  devtools: { enabled: true },
+
+  nitro: {
+    preset: 'cloudflare'
+  }
+})
