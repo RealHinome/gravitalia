266cbedcf167e077a36829f2438b6bf648445da1
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-10-21 16:18:20 +0200

    chore(nix): Remove useless packages

--- shell.nix
+++ shell.nix
@@ -1,15 +1,11 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  	nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.05";
+  	pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
 pkgs.mkShell {
 	buildInputs = with pkgs; [
 		cargo
-    darwin.apple_sdk.frameworks.Security
-    openssl_3
-    protobuf
-    pkg-config
-    rustc
-	];
+    		rustc
+  	];
 }
