2300202d83d16c4f51c21c588631b19f72b0f467
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-10-30 11:42:14 +0100

    feat: Add front

---
+++ front/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <NuxtLayout dark:bg-zinc-900 dark:text-white font-sans>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ front/pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useUser } from "../stores/user";
+
+const user = useUser();
+
+// Fetch user
+await user.fetchMe(false);
+</script>
+
+<template>
+    <p v-if="user.username !== ''" text-2xl ml-4>{{ $t("Hello {username}", { username: user.username }) }}</p>
+    <p v-else text-red-500 ml-4>{{ $t("You are not connected") }}</p>
+</template>
