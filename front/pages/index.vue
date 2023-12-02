2a366df68dadebe8c7e7f0d9b88b946839f4eba4
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-12-02 16:51:22 +0100

    feat: Update color

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -2,7 +2,7 @@
     <div flex flex-col items-center justify-center h-screen mx-auto>
         <h1 mr-2 text-4xl font-bold tracking-tight sm:text-6xl xl:text-6xl>
             <span block text-black dark:text-gray-100>Introducing...</span>
-            <span block text-purple-600 dark:text-purple-500>Gravitalia</span>
+            <span block text-violet-700 dark:text-violet-500>Gravitalia</span>
         </h1>
         
         <p mt-3 mr-12 xl:mr-36 text-base text-gray-400 sm:text-xl lg:text-lg xl:text-xl>
