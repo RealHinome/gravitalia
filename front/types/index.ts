92e0bc117f9b185b3b71827ad6279779b90536ae
Hinome <57831472+RealHinome@users.noreply.github.com>
2023-12-02 16:50:44 +0100

    chore: Remove comment and add locale

--- front/types/index.ts
+++ front/types/index.ts
@@ -1,14 +1,11 @@
-/**
-* Structure of a user
-*/
 export interface User {
     username: string;
     vanity: string;
     avatar?: string | null;
     bio?: string | null;
+    locale?: string | null;
     followers?: number | null;
     following?: string | null;
-    verified?: boolean | null;
     deleted?: boolean | null;
     flags?: number | null;
 }
